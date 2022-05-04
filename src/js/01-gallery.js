f4f9b88b721e8dfffc801a9c35d5ad07826b9ac4
ElenaBelaya <makseniya@ukr.net>
2022-05-04 20:00:35 +0300

    first task is not done yet

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import Simplelightbox from "simplelightbox";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(`.gallery`);
+
+function createGalleryItem(gallery) {
+    return gallery.map(({preview, original, description}) => {
+        return `
+        <a class="gallery__link" href="${original}">
+           <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    `}).join("");
+}
+
+const galleryCardMarkup = createGalleryItem(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryCardMarkup);
+
+galleryContainer.addEventListener(`click`, onViewingStart);
+
+let gallery = new Simplelightbox('.gallery a', {captionsData: "alt", captionDelay: 250});
+
+function onViewingStart(event) {
+    gallery.on('show.Simplelightbox')
+}
