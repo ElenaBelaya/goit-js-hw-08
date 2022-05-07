0d422d21b3e598b4014c34c6818574b4881f69e1
ElenaBelaya <makseniya@ukr.net>
2022-05-07 15:18:58 +0300

    lodash throttle

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,21 @@
+const formEl = document.querySelector(`.feedback-form`);
+console.log(formEl);
 
+const formTextEl = formEl.addEventListener(`input`, onFormSubmit);
+
+savedFormText();
+
+function onFormSubmit(event) {
+ event.preventDefault();
+ console.log(event.target.value);
+ localStorage.setItem("feedback-form-state", JSON.stringify({email: "event.target.value", message: ""}));
+ event.currentTarget.reset();
+}
+
+function savedFormText() {
+    const formText = localStorage.getItem("feedback-form-state");
+
+    if(formText) {
+     console.log(JSON.parse(formText));
+    }
+}
